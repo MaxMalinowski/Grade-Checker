34dfffe18a489e6b93de606449ba6e57b56e783b
Max Malinowski <max.malinowski@outlook.de>
2020-07-29 21:08:57 +0200

    cronjob improvment

--- start_up.sh
+++ start_up.sh
@@ -9,9 +9,12 @@ sudo apt install chromium-browser -y;
 sudo apt install chromium-chromedriver -y;
 
 rm -rf ./data.json
+mkdir logs
+touch logs/stdout.log
+touch logs/stderr.log
 
 pip3 install setuptools;
 pip3 install .;
 python3 main.py;
 
-crontab -l | { cat; echo "*/10 * * * * cd $PWD && $(which python3) ~/code/Grade-Checker/main.py"; } | crontab -
+crontab -l | { cat; echo "*/10 * * * * cd $PWD && $(which python3) ~/code/Grade-Checker/main.py" >$PWD/logs/stdout.log >$PWD/logs/stderr.log; } | crontab -
