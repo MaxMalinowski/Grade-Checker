f8babb8171d20fdc7561e012ccc9f388fba5e2b6
Max Malinowski <max.malinowski@outlook.de>
2020-07-29 21:12:21 +0200

    typo

--- start_up.sh
+++ start_up.sh
@@ -17,4 +17,4 @@ pip3 install setuptools;
 pip3 install .;
 python3 main.py;
 
-crontab -l | { cat; echo "*/10 * * * * cd $PWD && $(which python3) ~/code/Grade-Checker/main.py" >$PWD/logs/stdout.log >$PWD/logs/stderr.log; } | crontab -
+crontab -l | { cat; echo "*/10 * * * * cd $PWD && $(which python3) ~/code/Grade-Checker/main.py >$PWD/logs/stdout.log 2>$PWD/logs/stderr.log"; } | crontab -
