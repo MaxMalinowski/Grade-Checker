d7488b839cb2140099d4239700c0b0d89fab12a1
Max Malinowski <max.malinowski@outlook.de>
2020-07-29 19:03:57 +0200

    fix start_up script

--- start_up.sh
+++ start_up.sh
@@ -3,17 +3,14 @@
 sudo apt update;
 sudo apt upgrade -y;
 
-sudo apt install python3 python3-pip;
-sudo apt install chromium-chromedriver;
+sudo apt install python3 -y;
+sudo apt install python3-pip -y;
+sudo apt install chromium-chromedriver -y;
 
 rm -rf ./data.json
 
-python3 -m venv ./.venv;
-source .venv/bin/activate;
-
+pip3 install setuptools;
 pip3 install .;
 python3 main.py;
 
-deactivate;
-
-crontab -l | { cat; echo "*/10 * * * * source ~/code/Grade-Checker/.venv/bin/activate && $(which python3) ~/code/Grade-Checker/main.py && deactivate"; } | crontab -
+crontab -l | { cat; echo "*/10 * * * * $(which python3) ~/code/Grade-Checker/main.py"; } | crontab -
