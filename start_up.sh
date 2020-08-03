2f0c2b42959de2c4e571008f135e9315554d6f30
Max Malinowski <max.malinowski@outlook.de>
2020-08-03 17:55:40 +0200

    fix start_up.sh

--- start_up.sh
+++ start_up.sh
@@ -1,22 +1,23 @@
 #!/bin/sh
 
-sudo apt update;
-sudo apt upgrade -y;
+#sudo apt update;
+#sudo apt upgrade -y;
 
-sudo apt install python3 -y;
-sudo apt install python3-pip -y;
-sudo apt install chromium-browser -y;
-sudo apt install chromium-chromedriver -y;
+#sudo apt install python3 -y;
+#sudo apt install python3-pip -y;
+#sudo apt install chromium-browser -y;
+#sudo apt install chromium-chromedriver -y;
 
-rm -rf ./data.json
-mkdir logs
-touch logs/stdout.log
-touch logs/stderr.log
+#rm -rf ./data.json
+#mkdir logs
+#touch logs/stdout.log
+#touch logs/stderr.log
 
-pip3 install setuptools;
-pip3 install .;
-python3 main.py;
+#pip3 install setuptools;
+#pip3 install .;
+#python3 main.py;
 
-read -r "Enter time scheme of grade-checker cronjob (minute hour day month weakday): " timescheme
+echo -n "Enter time scheme of grade-checker cronjob (minute hour day month weakday): " 
+read timescheme
 
 crontab -l | { cat; echo "$timescheme cd $PWD && $(which python3) $PWD/main.py"; } | crontab -
